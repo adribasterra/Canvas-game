67ae9b67fde2d1be413c7f5e82222eb9dcc21b3f
adribasterra <adrianabasterraa@gmail.com>
2020-12-24 11:07:54 +0100

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,18 @@
+
+const gameHeight = 720;
+const gameWidth = 1080;
+
+function createFullScreenCanvas(){
+	//create the element
+	var canvas = document.createElement("canvas");
+	//make it fullscreen
+	canvas.width = gameWidth;
+	canvas.height = gameHeight;
+	canvas.style.position = "absolute";
+	// canvas.style.left = window.innerWidth / 2 - gameWidth / 2;
+	// canvas.style.top = window.innerHeight / 2 - gameHeight / 2;
+	
+	//add to the DOM
+	document.body.appendChild(canvas);
+	return canvas;
+}
